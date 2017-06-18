c43259682fe9f97c7c140445aaaa6abf9fc76864
Maniack Crudelis <maniack_crudelis@yahoo.fr>
2017-06-18 15:57:00 +0200

    Follow JimboJoe's notes

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 #=================================================
+# YUNOHOST MULTIMEDIA INTEGRATION
+#=================================================
+
+ynh_multimedia_build_main_dir () {
+	wget -qq https://github.com/YunoHost-Apps/yunohost.multimedia/archive/master.zip
+	unzip -qq master.zip
+	sudo ./yunohost.multimedia-master/script/ynh_media_build.sh
+}
+
+ynh_multimedia_addfolder () {
+	sudo ./yunohost.multimedia-master/script/ynh_media_addfolder.sh --source="$1" --dest="$2"
+}
+
+#=================================================
 #=================================================
 # TESTING
 #=================================================
