68ec4299b25787fafdb0f29b11590d455a59bb35
Félix Piédallu <felix.piedallu@non.se.com>
2023-12-06 11:25:05 +0100

    Also store the password hash on upgrade

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -30,6 +30,27 @@ _wait_and_save_rcp_password_hash() {
     return 1
 }
 
+
+_save_and_revert_rpc_password_hash_to_password() {
+    # This one is tricky :
+    # * transmission did password -> hash (we want to store hash)
+    # * user might have overrided other settings, we want to save them
+    # * we don't want false positives about user editing, so we revert the password change
+    # * in upgrade script, we will then re-write the saved password hash.
+
+    password_hash=$(jq -r '.["rpc-password"]' "$SETTINGS_FILE")
+    if [[ "$rpcpassword" == "$password_hash" ]]; then
+        # Upgrade already did this, exiting
+        return
+    fi
+
+    ynh_app_setting_set --app="$app" --key="rpcpassword" --value="$password_hash"
+
+    # Revert the change to maybe prevent ynh_backup_if_checksum_is_different to trigger
+    sed -i "s|\"${password_hash}\"|\"${rpcpassword}\"|" "$SETTINGS_FILE"
+}
+
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
